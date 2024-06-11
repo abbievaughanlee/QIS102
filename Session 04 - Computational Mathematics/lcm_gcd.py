5c9f53325e24f04630c0ebf827b9048b43c15ab0
abbievaughanlee <150642959+abbievaughanlee@users.noreply.github.com>
2024-06-11 16:02:18 -0500

    End of Session 04

--- Session 04 - Computational Mathematics/lcm_gcd.py
+++ Session 04 - Computational Mathematics/lcm_gcd.py
@@ -6,5 +6,14 @@ import numpy as np
 
 # function that will calculate lcm given two integer inputs
 
-    
 
+def lcm(x, y):
+    # store gcd of x and y
+    greatest = np.gcd(x, y)
+    # calculate lcm given gcd
+    lowest = (np.abs(x * y)) / greatest
+    print(lowest)
+
+
+# test
+lcm(447618, 2011835)
